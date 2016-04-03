154fd364a71399252a1926ea9581d38dce4378f7
terrydash <53047801@qq.com>
2016-04-03 13:11:48 +0800

    Redis链接缓存端可以使用了

---
+++ RedisCacheManager/RedisHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.Redis;
+
+namespace XgxFrameWork.CacheLayer.RedisCacheManager
+{
+    public static class RedisHelper
+    {
+        
+
+        public static string stringThings = string.Empty;
+        private static RedisClient Redis = new RedisClient("localhost",6379,"xgxxgx");//redis服务IP和端口
+        public static bool AddToRedis(string Key,string Value)
+        {
+
+            return Redis.Add(Key, Value);
+        }
+        public static bool GetStringFromRedis(string Key)
+        {
+            stringThings= Redis.Get(Key).ToString();
+            return  true;
+        }
+
+
+    }
+}
---
+++ XgxFrameWork.Presentation.Client.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XgxFrameWork.CacheLayer.RedisCacheManager;
+namespace XgxFrameWork.Presentation.Client.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Press a key followed by ENTER: ");
+            var key = (char)Console.Read(); // get a char
+            Console.Write("Press a value followed by ENTER: ");
+            var value = (char)Console.Read(); // get a char
+            RedisHelper.AddToRedis(key.ToString(), value.ToString());
+        }
+    }
+}
