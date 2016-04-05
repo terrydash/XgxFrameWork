39fd848c10c37ffde56211830f6e06720f25cda8
terrydash <53047801@qq.com>
2016-04-05 19:53:21 +0800

    接下来分解实体层到模板

--- XgxFrameWork.Presentation.Client.Console/Program.cs
+++ XgxFrameWork.Presentation.Client.Console/Program.cs
@@ -9,9 +9,9 @@ namespace XgxFrameWork.Presentation.Client.ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.Write("Press a key followed by ENTER: ");
+            Console.WriteLine("Press a key followed by ENTER: ");
             var key = (char)Console.Read(); // get a char
-            Console.Write("Press a value followed by ENTER: ");
+            Console.WriteLine("Press a value followed by ENTER: ");
             var value = (char)Console.Read(); // get a char
             RedisHelper.AddToRedis(key.ToString(), value.ToString());
         }
