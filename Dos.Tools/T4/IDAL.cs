00a19dc177cf98968b200dc8fb67f0b367e8a6ca
terry dash <mybest13@163.com>
2016-04-11 19:39:53 +0800

    准备开始实现T4模版的DAL层

---
+++ Dos.Tools/T4/IDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dos.ORM;
+namespace Dos.Tools.T4
+{
+    public interface IDAL
+    {        
+        //Entity GetEntityFromGuid(Guid guid);
+        Entity GetEntityFromID(int ID);
+        List<Entity> GetEntityListFromCuston(int ID);
+        List<Entity> GetEntityListFromCuston(int ID,int PageSize,int PageIndex);
+        
+    }
+}
